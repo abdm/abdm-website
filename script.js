3750691ed5ec301552be0cb6d38af248660ca4a0
abdm <abdmmulnivasiekta@gmail.com>
2024-12-15 22:34:57 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function sendEmail() {
     const recipient = "abdmmulnivasiekta@gmail.com"; // Recipient's email address
     const subject = encodeURIComponent("Request to Conduct Elections Using Ballot Papers"); // Subject of the email
     const body = encodeURIComponent(
-        `To the Honorable Central Election Commission,\n\nThe people of India are having doubts about the elections being conducted with EVMs in India. We are informing you of the opinions given by the people through e-mails that all future Legislative Assembly and Lok Sabha elections in the country should be conducted with "Ballot Papers" instead of EVMs.\n\nThank you.`
+        `To the Honorable Central Election Commission,\n\nThe people of India are having doubts about the elections being conducted with EVMs in India. We are informing you of the opinions given by the people through e-mails that all future Legislative Assembly and Lok Sabha elections in the country should be conducted with "Ballot Papers" instead of EVMs.\n\nThank you.\nADIVASI BC DALIT MINORITIES MULNIVASI EKTA ORGANISATION.\nTELANGANA STATE,INDIA.`
     ); // Body of the email
 
     // Construct the mailto link
